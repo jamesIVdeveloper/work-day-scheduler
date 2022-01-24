e56cc1a3540cae8fb274e25d497d3a1055cb6f7a
James Murphy <jamesivdeveloper@gmail.com>
2022-01-23 23:21:57 -0600

    add input for task

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
 // display current day in header
 var currentDay = moment().format("dddd, MMMM Do");
 $("#currentDay").html(currentDay);
+
+// save tasks to localStorage
+tasks = [];
+
+var saveTasks = function () {
+  localStorage.setItem("tasks", tasks);
+};
+
+$("#save-btn").on("click", function () {
+  console.log($("#task").textContent);
+});
